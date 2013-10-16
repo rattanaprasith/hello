807bcab760539db2ca523cc8309f64fec7a8eace
Rattana <rattana551@hotmail.com>
2013-10-16 13:20:37 +0200

    Preparing to tag version 0.1

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
   <title>Hello world</title>
 </header>
 <body>
-<h1>Hello</h1>
+<h1>Hello, ny version kommer snart</h1>
 <?php echo "Jag heter Rattana"; ?>
 </body>
 </html>
---
+++ hello.php~
@@ -0,0 +1,9 @@
+<html>
+<header>
+  <title>Hello world</title>
+</header>
+<body>
+<h1>Hello</h1>
+<?php echo "Jag heter Rattana"; ?>
+</body>
+</html>
